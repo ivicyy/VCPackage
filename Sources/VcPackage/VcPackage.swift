7b8aaee0ad9e75dfb8b50a7fde9ecee2824286c0
vc <ivicyy@sina.com>
2024-05-11 14:59:15 +0800

    base

--- Sources/VcPackage/VcPackage.swift
+++ Sources/VcPackage/VcPackage.swift
@@ -1,7 +1,7 @@
 import Alamofire
 import Foundation
 
-class HttpManage<T:Codable> {
+public class HttpManage<T:Codable> {
     public func requset(url:String,para:[String: Any],complete:@escaping(_ response:T)->(),failure:@escaping(_ error:Error)->()) {
         let headers: HTTPHeaders = ["accept": "application/json","Content-Type": "application/json",]
         AF.request(url,method: .post,parameters: para,encoding: JSONEncoding.default,headers: headers).responseDecodable(of:T.self) { resonse in
